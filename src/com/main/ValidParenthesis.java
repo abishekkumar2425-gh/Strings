8f4b76b99ce6cb40fa21d69e1ebb1dc291ba0db1
Abishek Kumar <abishekkumar2425@gmail.com>
2026-08-26 23:54:26 +0530

    Added Valid Parenthesis

---
+++ src/com/main/ValidParenthesis.java
@@ -0,0 +1,31 @@
+package com.main;
+import java.util.*;
+public class ValidParenthesis {
+
+	public static void main(String[] args) {
+		String str = "({[]}){}";
+		char top = 0;
+		Deque<Character> Stack = new ArrayDeque<>();
+		for(char c: str.toCharArray()) {
+			if( c == '(' || c == '{' || c == '[') {
+				Stack.push(c);
+			}
+			else {
+			   top = Stack.pop();
+			}
+			if((c == ')' && top != '(')||
+			  (c == '}' && top != '{')||
+			  (c == ']' && top != '[')) {
+				System.out.println("Invalid Parenthesis");
+				return ;
+			}
+		}
+		if(Stack.isEmpty()) {
+			System.out.println("Valid Parenthesis");
+		}
+		else {
+			System.out.println("Invalid Parenthesis");
+		}
+	}
+
+}
